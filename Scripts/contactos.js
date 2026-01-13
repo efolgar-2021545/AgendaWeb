e6e725f10ed9eb872c1665a4c906c38a8eb993c8
efolgar-2021545 <efolgar-2021545@kinal.edu.gt>
2026-01-12 18:07:52 -0600

    feat: Se completo el funcionamiento de la interfaz Contactos

---
+++ Scripts/contactos.js
@@ -0,0 +1,6 @@
+// Función del click para dar los detalles de los contactos
+function verDetalle(id) {
+    window.location.href = 'detalles.html?id=' + id;
+}
+
+console.log("Módulo de contactos cargado.");
