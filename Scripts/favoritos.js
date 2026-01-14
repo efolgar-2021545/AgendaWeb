209b0cf0782c7fb9eca93fc445cea82f8843234c
efolgar-2021545 <efolgar-2021545@kinal.edu.gt>
2026-01-14 17:07:57 -0600

    feat: Se completo el funcionamiento de la interfaz de Favoritos

---
+++ Scripts/favoritos.js
@@ -0,0 +1,5 @@
+// scripts/favoritos.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Sección de favoritos cargada correctamente.");
+    console.log("Mostrando 2 contactos destacados como requiere el punto 5.");
+});
